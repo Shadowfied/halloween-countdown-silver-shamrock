1cd28bd9c9561d3b7aaf0c413e38c8d94d7437bb
Shadowfied <admin@shadowfied.com>
2022-10-16 21:35:00 +0200

    chore(release): 1.0.0
    1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const audio = useRef<HTMLAudioElement>(null);
+  const video = useRef<HTMLVideoElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const onClick = () => {
+    setIsPlaying(!isPlaying);
+    audio.current?.paused ? audio.current?.play() : audio.current?.pause();
+    video.current?.paused ? video.current.play() : video.current?.pause();
+  };
+
+  useEffect(() => {
+    if (video.current) {
+      video.current.currentTime = 0.2;
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' onClick={onClick} style={{ cursor: 'pointer' }}>
+      <audio
+        ref={audio}
+        src={`/assets/audio/${
+          31 -
+          Number(new Date().toLocaleDateString(undefined, { day: 'numeric' }))
+        }.mp3`}
+        loop
+        playsInline
+      />
+      <video
+        ref={video}
+        src='/assets/video/vid.mp4'
+        loop
+        playsInline
+        poster='/assets/images/poster.jpg'
+      />
+      {!isPlaying && (
+        <>
+          <div className='overlay' />
+          <div className='click-to-play'>▶️</div>
+        </>
+      )}
     </div>
   );
 }

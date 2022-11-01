b387747f4cbf41ec2f668dcc4d82f2d18b218f67
Shadowfied <admin@shadowfied.com>
2022-11-01 22:56:11 +0100

    feat: add support for all days

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,21 @@ function App() {
   const video = useRef<HTMLVideoElement>(null);
   const [isPlaying, setIsPlaying] = useState(false);
 
+  const currentMonth = new Date().toLocaleDateString(undefined, {
+    month: 'numeric',
+  });
+  const currentYear = new Date().getUTCFullYear();
+  const currentDay = new Date().toLocaleDateString(undefined, {
+    day: 'numeric',
+  });
+  const yearToCountTo =
+    Number(currentMonth) <= 10 ? currentYear : currentYear + 1;
+
+  const nextHalloween = new Date(`10/31/${yearToCountTo}`);
+
+  const timeBetween = nextHalloween.getTime() - new Date().getTime();
+  const daysBetween = Math.ceil(timeBetween / (1000 * 3600 * 24));
+
   const onClick = () => {
     setIsPlaying(!isPlaying);
     audio.current?.paused ? audio.current?.play() : audio.current?.pause();
@@ -19,13 +34,15 @@ function App() {
   }, []);
 
   return (
-    <div className='App' onClick={onClick} style={{ cursor: 'pointer' }}>
+    <div
+      className='App'
+      onClick={onClick}
+      style={{ cursor: 'pointer' }}
+      key={currentDay}
+    >
       <audio
         ref={audio}
-        src={`/assets/audio/${
-          31 -
-          Number(new Date().toLocaleDateString(undefined, { day: 'numeric' }))
-        }.mp3`}
+        src={`/assets/audio/${daysBetween}.mp3`}
         loop
         playsInline
       />
